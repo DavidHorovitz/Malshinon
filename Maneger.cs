7c7d840bc63e6a9a6e7e7d68641deba34f28f614
david horovitz <dovidho@gmail.com>
2025-06-11 10:17:41 +0300

    almost part 1

--- Maneger.cs
+++ Maneger.cs
@@ -8,11 +8,85 @@ namespace Malshinon
 {
     internal class Maneger
     {
-        People people = new People("avi", "cohen");
+        People people = new People();
+        DAL dAL = new DAL();
+
+        public string getFirstNameOfReporter ()
+        {
+            Console.WriteLine("'enter your first name");
+            string first_name = Console.ReadLine();
+            validation(first_name);
+            return first_name;
+        }
+        public string getLastNameOfReporter()
+        {
+            Console.WriteLine("'enter your last name");
+            string last_name = Console.ReadLine();
+            validation(last_name);
+            return last_name;
+        }
+        public string getFirstNameOfTarget()
+        {
+            Console.WriteLine("'enter target first name");
+            string first_name = Console.ReadLine();
+            validation(first_name);
+            return first_name;
+        }
+        public string getLastNameOfTarget()
+        {
+            Console.WriteLine("'enter target last name");
+            string last_name = Console.ReadLine();
+            validation(last_name);
+            return last_name;
+        }
+
+        public string getInformation()
+        {
+            Console.WriteLine("please enter the information");
+            string infomation = Console.ReadLine();
+            validation(infomation);
+            return infomation;
+        }
+
+        public string validation(string text)
+        {
+                while (text == "")
+                {
+                    Console.WriteLine("enter again");
+                    text = Console.ReadLine();
+                }
+                return text;
+          
+        }
         public void starter ()
         {
-            people.checkMalshin("avi", "moshe");
-            people.checkTarget("avi", "moshe");
+           
+            string get_FirstNameOfReporter = getFirstNameOfReporter();
+            string get_FirstNameOfTarget = getFirstNameOfTarget();
+
+            bool reporter = people.checkMalshin(get_FirstNameOfReporter);
+            bool target = people.checkTarget(get_FirstNameOfTarget);
+
+            if (reporter && target)
+            {
+                dAL.insertReports(get_FirstNameOfReporter, get_FirstNameOfTarget, getInformation());
+            }
+            else if (reporter)
+            {
+                dAL.InsertPeople(get_FirstNameOfTarget, getLastNameOfTarget());
+                dAL.insertReports(get_FirstNameOfReporter, get_FirstNameOfTarget, getInformation());
+            }
+            else if (target)
+            {
+                dAL.InsertPeople(get_FirstNameOfReporter, getLastNameOfReporter());
+                dAL.insertReports(get_FirstNameOfReporter, get_FirstNameOfTarget, getInformation());
+            }
+            else
+            {
+                dAL.InsertPeople(get_FirstNameOfReporter, getLastNameOfReporter());
+                dAL.InsertPeople(get_FirstNameOfTarget, getLastNameOfTarget());
+                dAL.insertReports(get_FirstNameOfReporter, get_FirstNameOfTarget, getInformation());
+            }
         }
     }
 }
